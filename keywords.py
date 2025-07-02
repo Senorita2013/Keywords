e34052879a4fb00d175bc8afe7479bbb30b51f35
Senorita2013 <iamsenoritamishra@gmail.com>
2025-07-02 17:30:22 +0100

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,36 @@
+#break keyword
+a=input("Enter a word:")
+
+for i in a:
+    if (i=='A'):
+        print("A is found")
+        break
+    else:
+        print("A is not found")
+
+#pass keyword
+for x in range(10):
+    if x % 20 == 0:
+        print("twist")
+
+    elif x % 15 == 0:
+        pass
+
+    elif x % 5 == 0:
+        print("fizz")
+
+    elif x % 3 == 0:
+        print("buzz")
+
+    else:
+        print (x)
+
+#continue keyword
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+
+    print('\nCurrent variable value:',var)
+print ("\nGood bye!")
